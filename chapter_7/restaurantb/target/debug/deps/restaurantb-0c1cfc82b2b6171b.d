3cbfc0054d63d7cd943dc3387bfb628799461d23
yson <magoyeison@gmail.com>
2020-11-24 20:16:13 -0400

    chapter 7 remove target folder

--- chapter_7/restaurantb/target/debug/deps/restaurantb-0c1cfc82b2b6171b.d
+++
@@ -1,7 +0,0 @@
-C:\Users\ysonpc\Documents\codes\rust\rust_practice_book\chapter_7\restaurantb\target\debug\deps\restaurantb-0c1cfc82b2b6171b.rmeta: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
-
-C:\Users\ysonpc\Documents\codes\rust\rust_practice_book\chapter_7\restaurantb\target\debug\deps\restaurantb-0c1cfc82b2b6171b.d: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
-
-src\lib.rs:
-src\front_of_house.rs:
-src\front_of_house\hosting.rs:
