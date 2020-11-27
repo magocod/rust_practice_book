5dd1f8f1e01f968cd9d91f47c5b1cc42e39e9b91
yson <magoyeison@gmail.com>
2020-11-27 19:06:49 -0400

    chapter 10

---
+++ chapter_10/pair/src/main.rs
@@ -0,0 +1,29 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+fn main() {
+    let p = Pair::new(3, 2);
+    println!("{:#?}", p);
+    p.cmp_display()
+}
