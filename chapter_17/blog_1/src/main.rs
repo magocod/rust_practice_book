8067d7c4556583e7158e78ce9fbb6bbfb1b96860
yson <magoyeison@gmail.com>
2020-12-03 11:33:19 -0400

    chapter 17 poo

---
+++ chapter_17/blog_1/src/main.rs
@@ -0,0 +1,17 @@
+use blog_1::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    let view: &str = &post.content()[..];
+    println!("{}", view);
+}
