0c7b66e2b68211debd46063e0857b52f613dcc3f
magocod <magoyeison@gmail.com>
2021-01-02 18:44:04 -0400

    zero_cost

---
+++ typestates/zero_cost/src/main.rs
@@ -0,0 +1,25 @@
+pub struct PA0;
+pub struct PA1;
+// ...
+
+pub struct PortA;
+
+impl PortA {
+    pub fn split(self) -> PortAPins {
+        PortAPins {
+            pa0: PA0,
+            pa1: PA1,
+            // ...
+        }
+    }
+}
+
+pub struct PortAPins {
+    pub pa0: PA0,
+    pub pa1: PA1,
+    // ...
+}
+
+fn main() {
+    println!("Hello, world!");
+}
