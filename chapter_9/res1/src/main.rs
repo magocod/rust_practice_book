0b35246e5913614fe95df8323da4157c16549aeb
magocod <magoyeison@gmail.com>
2021-01-17 20:53:24 -0400

    chapter 9

---
+++ chapter_9/res1/src/main.rs
@@ -0,0 +1,22 @@
+use std::num::ParseIntError;
+
+fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    let first_number = first_number_str.parse::<i32>()?;
+    let second_number = second_number_str.parse::<i32>()?;
+
+    Ok(first_number * second_number)
+}
+
+fn print(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n)  => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    let r1 = multiply("10", "2");
+    let r2 = multiply("t", "2");
+    print(r1);
+    print(r2);
+}
