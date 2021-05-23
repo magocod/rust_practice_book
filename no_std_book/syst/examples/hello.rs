ad848f03f38c63c2f632e8854ee15c1ea590c3c3
magocod <magoyeison@gmail.com>
2021-05-23 18:38:24 -0400

    async examples, no_std examples

--- no_std_book/syst/examples/hello.rs
+++ no_std_book/syst/examples/hello.rs
@@ -8,9 +8,6 @@ use panic_halt as _;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
 
-// qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -semihosting-config enable=on,target=native -kernel target/thumbv7m-none-eabi/debug/examples/hello
-// qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -semihosting-config enable=on,target=native -gdb tcp::3333 -S -kernel target/thumbv7m-none-eabi/debug/examples/hello
-
 #[entry]
 fn main() -> ! {
     hprintln!("Hello, world!").unwrap();
