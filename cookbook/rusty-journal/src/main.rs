a558d94edaf4233c6c970edff8b8d2e6e39a22f1
magocod <magoyeison@gmail.com>
2021-10-23 19:37:10 -0400

    rusty journal finish

--- cookbook/rusty-journal/src/main.rs
+++ cookbook/rusty-journal/src/main.rs
@@ -1,12 +1,64 @@
-mod cli;
-mod tasks;
 use structopt::StructOpt;
 
-fn main() {
-    // cargo run -- add "buy milk"
-    // cargo run -- done 4
-    // cargo run -- -j groceries.txt list
+mod cli;
+mod tasks;
 
-    // cli::CommandLineArgs::from_args();
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
+use std::path::PathBuf;
+
+use anyhow::anyhow;
+
+// cargo run -- -j test-journal.json add "buy milk"
+// cargo run -- -j test-journal.json add "take the dog for a walk"
+// cargo run -- -j test-journal.json list
+// cargo run -- -j test-journal.json done 2
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
+
+// fn main() {
+//     // Get the command-line arguments.
+//     let CommandLineArgs {
+//         action,
+//         journal_file,
+//     } = CommandLineArgs::from_args();
+//
+//     // Unpack the journal file.
+//     // let journal_file = journal_file.expect("Failed to find journal file");
+//
+//     let journal_file = journal_file
+//         .or_else(find_default_journal_file)
+//         .expect("Failed to find journal file.");
+//
+//     // Perform the action.
+//     match action {
+//         Add { text } => tasks::add_task(journal_file, Task::new(text)),
+//         List => tasks::list_tasks(journal_file),
+//         Done { position } => tasks::complete_task(journal_file, position),
+//     }
+//         .expect("Failed to perform action")
+// }
+
+fn main() -> anyhow::Result<()> {
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .ok_or(anyhow!("Failed to find journal file."))?;
+
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }?;
+    Ok(())
 }
