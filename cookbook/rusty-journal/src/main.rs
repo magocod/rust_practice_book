359df7cc06855170f10c15f700c84b19a780f606
magocod <magoyeison@gmail.com>
2021-10-22 20:28:37 -0400

    cook book rusty journal

---
+++ cookbook/rusty-journal/src/main.rs
@@ -0,0 +1,11 @@
+mod cli;
+use structopt::StructOpt;
+
+fn main() {
+    // cargo run -- add "buy milk"
+    // cargo run -- done 4
+    // cargo run -- -j groceries.txt list
+
+    // cli::CommandLineArgs::from_args();
+    println!("{:#?}", cli::CommandLineArgs::from_args());
+}
