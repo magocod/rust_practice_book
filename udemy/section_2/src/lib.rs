1927079da67a2427b01691d630de75274478894a
magocod <magoyeison@gmail.com>
2022-04-03 15:19:20 -0400

    udemy section_2

---
+++ udemy/section_2/src/lib.rs
@@ -0,0 +1,47 @@
+pub mod combi;
+pub mod gen_range;
+pub mod into;
+
+pub struct RangeIterator {
+    curr: i32,
+    stop: i32,
+    step: i32,
+}
+
+impl RangeIterator {
+    pub fn new(start: i32, stop: i32, step: i32) -> Self {
+        RangeIterator {
+            curr: start,
+            stop,
+            step,
+        }
+    }
+}
+
+impl Iterator for RangeIterator {
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        // println!("before, {}", self.curr);
+        if self.curr >= self.stop {
+            return None;
+        }
+        // println!("after, {}", self.curr);
+        let res = self.curr;
+        self.curr += self.step;
+        Some(res)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let mut m = 0;
+        let it = RangeIterator::new(5, 12, 3);
+        for s in it {
+            m += s;
+        }
+        assert_eq!(m, 5 + 8 + 11);
+    }
+}
